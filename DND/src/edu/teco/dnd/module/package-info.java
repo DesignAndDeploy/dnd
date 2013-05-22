38684259e2d3de7a2baa0163d24d6aa161cc0fdb
cryptkiddy <cryptkiddy@archkiddy.lan>
2013-05-22 23:34:30 +0200

    improve javadoc

---
+++ DND/src/edu/teco/dnd/module/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains the classes running directly on the Module. It also provides the
+ * configuration used for deploying applications.
+ */
+package edu.teco.dnd.module;
+
