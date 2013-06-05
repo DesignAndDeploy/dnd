bc41bfd570261027b646a4e925d2ec856e0f291b
Philipp Adolf <adolf@teco.edu>
2013-06-05 16:00:39 +0200

    Add startServer, shutdownServer to Activator

--- DND/src/edu/teco/dnd/eclipse/Activator.java
+++ DND/src/edu/teco/dnd/eclipse/Activator.java
@@ -1,6 +1,7 @@
 package edu.teco.dnd.eclipse;
 
 import io.netty.bootstrap.ChannelFactory;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.oio.OioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -12,8 +13,12 @@ import java.net.InetSocketAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -27,6 +32,7 @@ import edu.teco.dnd.network.UDPMulticastBeacon;
 import edu.teco.dnd.network.logging.Log4j2LoggerFactory;
 import edu.teco.dnd.util.NetConnection;
 
+// TODO: EventLoopGroups are not stopped. Probably need a way to listen for ConnectionManager/UDPMulticastBeacon
 public class Activator extends AbstractUIPlugin {
 	private static final Logger LOGGER = LogManager.getLogger(Activator.class);
 	
@@ -38,6 +44,10 @@ public class Activator extends AbstractUIPlugin {
 
 	private UDPMulticastBeacon beacon;
 	
+	private final Set<DNDServerStateListener> serverStateListener = new HashSet<DNDServerStateListener>();
+	
+	private final ReadWriteLock serverStateLock = new ReentrantReadWriteLock();
+	
 	
 	static {
 		InternalLoggerFactory.setDefaultFactory(new Log4j2LoggerFactory());
@@ -48,42 +58,85 @@ public class Activator extends AbstractUIPlugin {
 		return plugin;
 	}
 	
+	@Override
+	public void start(final BundleContext context) throws Exception {
+		LOGGER.entry(context);
+		super.start(context);
+		plugin = this;
+		
+		uuid = UUID.randomUUID();
+		
+		LOGGER.exit();
+	}
+	
+	@Override
+	public void stop(final BundleContext context) throws Exception {
+		LOGGER.entry();
+		super.stop(context);
+		plugin = null;
+
+		LOGGER.exit();
+	}
+	
 	public void startServer() {
 		LOGGER.entry();
+		serverStateLock.readLock().lock();
+		try {
+			if (connectionManager != null && !connectionManager.isShutDown()) {
+				LOGGER.exit();
+				return;
+			}
+		} finally {
+			serverStateLock.readLock().unlock();
+		}
+		
+		TCPConnectionManager connectionManager = null;
+		EventLoopGroup networkEventLoopGroup = null;
 		final List<InetSocketAddress> listen = getListen();
 		final List<InetSocketAddress> announce = getAnnounce();
 		final List<NetConnection> multicast = getMulticast();
-		
-		final NioEventLoopGroup networkEventLoopGroup = new NioEventLoopGroup();
-
-		final TCPConnectionManager connectionManager = new TCPConnectionManager(networkEventLoopGroup,
-				networkEventLoopGroup, new ChannelFactory<NioServerSocketChannel>() {
-					@Override
-					public NioServerSocketChannel newChannel() {
-						return new NioServerSocketChannel();
-					}
-				}, new ChannelFactory<NioSocketChannel>() {
-					@Override
-					public NioSocketChannel newChannel() {
-						return new NioSocketChannel();
-					}
-				}, uuid);
-		this.connectionManager = connectionManager;
-
-		beacon = new UDPMulticastBeacon(new ChannelFactory<OioDatagramChannel>() {
-			@Override
-			public OioDatagramChannel newChannel() {
-				return new OioDatagramChannel();
+			
+		serverStateLock.writeLock().lock();
+		try {
+			if (this.connectionManager != null && !this.connectionManager.isShutDown()) {
+				LOGGER.exit();
+				return;
 			}
-		}, new OioEventLoopGroup(), networkEventLoopGroup, uuid);
+
+			networkEventLoopGroup = new NioEventLoopGroup();
+		
+			connectionManager = new TCPConnectionManager(networkEventLoopGroup,
+					networkEventLoopGroup, new ChannelFactory<NioServerSocketChannel>() {
+						@Override
+						public NioServerSocketChannel newChannel() {
+							return new NioServerSocketChannel();
+						}
+					}, new ChannelFactory<NioSocketChannel>() {
+						@Override
+						public NioSocketChannel newChannel() {
+							return new NioSocketChannel();
+						}
+					}, uuid);
+			this.connectionManager = connectionManager;
+		
+			beacon = new UDPMulticastBeacon(new ChannelFactory<OioDatagramChannel>() {
+				@Override
+				public OioDatagramChannel newChannel() {
+					return new OioDatagramChannel();
+				}
+			}, new OioEventLoopGroup(), networkEventLoopGroup, uuid);
+		} finally {
+			serverStateLock.writeLock().unlock();
+		}
 		beacon.addListener(connectionManager);
 		beacon.setAnnounceAddresses(announce);
-
+	
+		final TCPConnectionManager conMan = connectionManager;
 		networkEventLoopGroup.execute(new Runnable() {
 			@Override
 			public void run() {
 				for (final InetSocketAddress address : listen) {
-					connectionManager.startListening(address);
+					conMan.startListening(address);
 				}
 				for (final NetConnection netConnection : multicast) {
 					beacon.addAddress(netConnection.getInterface(), netConnection.getAddress());
@@ -92,6 +145,88 @@ public class Activator extends AbstractUIPlugin {
 		});
 		
 		LOGGER.exit();
+	}
+
+	public void shutdownServer() {
+		LOGGER.entry();
+		serverStateLock.readLock().lock();
+		try {
+			if (connectionManager == null || connectionManager.isShuttingDown()) {
+				LOGGER.exit();
+				return;
+			}
+		} finally {
+			serverStateLock.readLock().unlock();
+		}
+		
+		serverStateLock.writeLock().lock();
+		try {
+			if (connectionManager == null || connectionManager.isShuttingDown()) {
+				return;
+			}
+			connectionManager.shutdown();
+			beacon.shutdown();
+			for (final DNDServerStateListener listener : serverStateListener) {
+				listener.serverStopped();
+			}
+		} finally {
+			serverStateLock.writeLock().unlock();
+		}
+		LOGGER.exit();
+	}
+
+	public ConnectionManager getConnectionManager() {
+		return connectionManager;
+	}
+
+	public UDPMulticastBeacon getMulticastBeacon() {
+		return beacon;
+	}
+
+	@Override
+	protected void initializeDefaultPreferences(IPreferenceStore store) {
+		store.setDefault("listen", "bli");
+		store.setDefault("multicast", "bla");
+		store.setDefault("announce", "blubb");
+	}
+	
+	public void addServerStateListener(final DNDServerStateListener listener) {
+		serverStateLock.writeLock().lock();
+		try {
+			serverStateListener.add(listener);
+			if (connectionManager == null) {
+				listener.serverStopped();
+			} else {
+				listener.serverStarted(connectionManager, beacon);
+			}
+		} finally {
+			serverStateLock.writeLock().unlock();
+		}
+	}
+	
+	public void removeServerStateListener(final DNDServerStateListener listener) {
+		serverStateLock.writeLock().lock();
+		try {
+			serverStateListener.remove(listener);
+		} finally {
+			serverStateLock.writeLock().unlock();
+		}
+	}
+	
+	public boolean isRunning() {
+		LOGGER.entry();
+		serverStateLock.readLock().lock();
+		try {
+			if (connectionManager == null || connectionManager.isShuttingDown()) {
+				LOGGER.exit(false);
+				return false;
+			} else {
+				LOGGER.exit(true);
+				return true;
+			}
+		} finally {
+			serverStateLock.readLock().unlock();
+		}
 	}
 
 	private List<InetSocketAddress> getListen() {
@@ -142,46 +277,5 @@ public class Activator extends AbstractUIPlugin {
 			}
 		}
 		return addresses;
-	}
-	
-	@Override
-	public void start(final BundleContext context) throws Exception {
-		LOGGER.entry(context);
-		super.start(context);
-		plugin = this;
-		
-		uuid = UUID.randomUUID();
-		
-		startServer();
-		LOGGER.exit();
-	}
-	
-	@Override
-	public void stop(final BundleContext context) throws Exception {
-		LOGGER.entry();
-		super.stop(context);
-		plugin = null;
-
-		// TODO: shutdown
-		connectionManager.shutdown();
-		while (!connectionManager.isShutDown()) {
-			Thread.sleep(100);
-		}
-		LOGGER.exit();
-	}
-
-	public ConnectionManager getConnectionManager() {
-		return connectionManager;
-	}
-
-	public UDPMulticastBeacon getMulticastBeacon() {
-		return beacon;
-	}
-
-	@Override
-	protected void initializeDefaultPreferences(IPreferenceStore store) {
-		store.setDefault("listen", "bli");
-		store.setDefault("multicast", "bla");
-		store.setDefault("announce", "blubb");
 	}
 }
---
+++ DND/src/edu/teco/dnd/eclipse/DNDServerStateListener.java
@@ -0,0 +1,15 @@
+package edu.teco.dnd.eclipse;
+
+import edu.teco.dnd.network.ConnectionManager;
+import edu.teco.dnd.network.UDPMulticastBeacon;
+
+/**
+ * Gets informed if the server components are started or stopped.
+ *
+ * @author Philipp Adolf
+ */
+public interface DNDServerStateListener {
+	void serverStarted(ConnectionManager connectionManager, UDPMulticastBeacon beacon);
+	
+	void serverStopped();
+}
