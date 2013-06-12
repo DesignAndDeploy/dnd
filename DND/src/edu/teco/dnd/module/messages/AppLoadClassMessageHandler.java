30a5cdd42f8c9fa049a805a11640e413b416146d
Philipp Adolf <adolf@teco.edu>
2013-06-12 16:59:55 +0200

    Remove whitespace

--- DND/src/edu/teco/dnd/module/messages/AppLoadClassMessageHandler.java
+++ DND/src/edu/teco/dnd/module/messages/AppLoadClassMessageHandler.java
@@ -6,7 +6,7 @@ import edu.teco.dnd.module.Application;
 import edu.teco.dnd.module.ModuleApplicationManager;
 import edu.teco.dnd.network.MessageHandler;
 
-public class AppLoadClassMessageHandler  implements MessageHandler<AppLoadClassMessage> {
+public class AppLoadClassMessageHandler implements MessageHandler<AppLoadClassMessage> {
 	final ModuleApplicationManager appManager;
 	final Application associatedApp;
 	
