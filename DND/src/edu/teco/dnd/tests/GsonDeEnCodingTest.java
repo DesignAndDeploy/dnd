4bc117556d92b0e1c483039a1f1870d24751dc6f
Marvin <cryptkiddy@gmail.com>
2013-07-09 15:40:05 +0200

    improve gson testcode a little.

--- DND/src/edu/teco/dnd/tests/GsonDeEnCodingTest.java
+++ DND/src/edu/teco/dnd/tests/GsonDeEnCodingTest.java
@@ -1,6 +1,8 @@
 package edu.teco.dnd.tests;
 
 import java.net.InetSocketAddress;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 import org.apache.logging.log4j.LogManager;
@@ -8,10 +10,11 @@ import org.apache.logging.log4j.Logger;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.sun.corba.se.impl.protocol.giopmsgheaders.Message;
 
+import edu.teco.dnd.module.messages.killApp.KillAppAck;
 import edu.teco.dnd.module.messages.killApp.KillAppMessage;
-import edu.teco.dnd.network.messages.ApplicationSpecificMessage;
+import edu.teco.dnd.network.codecs.MessageAdapter;
+import edu.teco.dnd.network.messages.Message;
 import edu.teco.dnd.util.Base64Adapter;
 import edu.teco.dnd.util.InetSocketAddressAdapter;
 import edu.teco.dnd.util.NetConnection;
@@ -19,38 +22,60 @@ import edu.teco.dnd.util.NetConnectionAdapter;
 
 public class GsonDeEnCodingTest {
 
-	private final static  UUID TEST_UUID = UUID.randomUUID(); 
-	private static transient final Logger LOGGER = LogManager.getLogger(GsonDeEnCodingTest.class);
-	private static transient final Gson gson;
+	private final static UUID TEST_UUID = UUID.randomUUID();
+	private static final Logger LOGGER = LogManager.getLogger(GsonDeEnCodingTest.class);
+	private static final Gson gson;
+	private static final MessageAdapter msgAdapter = new MessageAdapter();
 	static {
 		GsonBuilder builder = new GsonBuilder();
 		builder.setPrettyPrinting();
+		builder.registerTypeAdapter(Message.class,msgAdapter);
 		builder.registerTypeAdapter(InetSocketAddress.class, new InetSocketAddressAdapter());
 		builder.registerTypeAdapter(NetConnection.class, new NetConnectionAdapter());
 		builder.registerTypeAdapter(byte[].class, new Base64Adapter());
 		gson = builder.create();
 	}
-	
+
 	public static void main(String[] args) throws Exception {
-		ApplicationSpecificMessage testMsg = new KillAppMessage(TEST_UUID);
+		Set<Message> testMsgs = new HashSet<Message>();
+		testMsgs.add(new KillAppMessage(TEST_UUID));
+		msgAdapter.addMessageType(KillAppMessage.class, "killAppMsg");
 		
-		try {
-		testEnDeCoding(testMsg);
-		} catch (Exception ex) {
-			LOGGER.fatal("Error while de/encoding msg: " + testMsg);
-			throw ex;
+		testMsgs.add(new KillAppAck(new KillAppMessage(TEST_UUID)));
+
+		for (Message msg : testMsgs) {
+			testEnDeCoding(msg);
 		}
 	}
-	
-	public static void testEnDeCoding(ApplicationSpecificMessage msg) {
-		LOGGER.entry(msg);
-		String gsonHolder = gson.toJson(msg);
-		if(!msg.equals(gson.fromJson(gsonHolder, Message.class))) {
-			LOGGER.fatal("Decoded " + msg + " wrong.(Or messages equal methode is broken)");
+
+	public static void testEnDeCoding(Message msg) {
+		String gsonHolder;
+		Message decodedMsg;
+
+		try {
+			gsonHolder = gson.toJson(msg);
+		} catch (Exception ex) {
+			LOGGER.fatal(msg);
+			LOGGER.fatal("Encoding Error.");
+			LOGGER.throwing(ex);
+			throw new Error(ex);
+		}
+
+		try {
+			decodedMsg = gson.fromJson(gsonHolder, Message.class);
+		} catch (Exception ex) {
+			LOGGER.fatal(msg);
+			LOGGER.fatal("Decoding Error.");
+			LOGGER.throwing(ex);
+			throw new Error(ex);
+		}
+
+		if (!msg.equals(decodedMsg)) {
+			LOGGER.fatal("Decoded " + msg + " wrong.(Or messages equal methode is broken)\nWas decoded to: "
+					+ decodedMsg);
 		} else {
 			LOGGER.info("Succefull de/encoded msg: " + msg);
 		}
-		
-	}
 
+	}
 }
