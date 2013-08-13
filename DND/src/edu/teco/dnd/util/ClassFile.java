d2d85dae1d49383b465480c640e324546db59e17
Philipp Adolf <adolf@teco.edu>
2013-08-13 15:02:19 +0200

    Add hashCode, equals to ClassFile

--- DND/src/edu/teco/dnd/util/ClassFile.java
+++ DND/src/edu/teco/dnd/util/ClassFile.java
@@ -51,4 +51,36 @@ public class ClassFile {
 	public String toString() {
 		return "ClassFile[className=" + className + ",file=" + file + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((className == null) ? 0 : className.hashCode());
+		result = prime * result + ((file == null) ? 0 : file.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ClassFile other = (ClassFile) obj;
+		if (className == null) {
+			if (other.className != null)
+				return false;
+		} else if (!className.equals(other.className))
+			return false;
+		if (file == null) {
+			if (other.file != null)
+				return false;
+		} else if (!file.equals(other.file))
+			return false;
+		return true;
+	}
 }
