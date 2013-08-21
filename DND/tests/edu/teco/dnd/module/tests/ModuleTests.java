a862295f28d857abb18829491484f82f32e62828
Philipp Adolf <adolf@teco.edu>
2013-08-21 13:45:16 +0200

    Add test suites

---
+++ DND/tests/edu/teco/dnd/module/tests/ModuleTests.java
@@ -0,0 +1,11 @@
+package edu.teco.dnd.module.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ GsonDeEnCodingTest.class })
+public class ModuleTests {
+
+}
