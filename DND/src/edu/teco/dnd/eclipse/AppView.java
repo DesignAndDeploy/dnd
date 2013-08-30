3c2e4bc2c762debe8677835266732b851d07d7cb
Alisa Jung <jung@teco.edu>
2013-08-30 15:41:44 +0200

    started working on Application View. Not done yet. Had some problems with deploying a simple application - took a very long time, did abort.

--- DND/src/edu/teco/dnd/eclipse/AppView.java
+++ DND/src/edu/teco/dnd/eclipse/AppView.java
@@ -1,11 +1,35 @@
 package edu.teco.dnd.eclipse;
 
+
+import java.util.Collection;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.ui.IMemento;
+import org.eclipse.ui.IViewSite;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.part.ViewPart;
+
+import edu.teco.dnd.discover.ApplicationInformation;
+import edu.teco.dnd.module.Application;
+import edu.teco.dnd.network.ConnectionManager;
+import edu.teco.dnd.network.UDPMulticastBeacon;
+import edu.teco.dnd.server.ApplicationManager;
+import edu.teco.dnd.server.ApplicationManagerListener;
+import edu.teco.dnd.server.DNDServerStateListener;
+import edu.teco.dnd.server.ServerManager;
 
 /**
  * View for the applications / running function blocks.
@@ -13,8 +37,39 @@ import org.eclipse.ui.part.ViewPart;
  * @author jung
  * 
  */
-public class AppView extends ViewPart {
-	private Label label;
+public class AppView extends ViewPart implements ApplicationManagerListener {
+
+	/**
+	 * The logger for this class.
+	 */
+	private static final Logger LOGGER = LogManager.getLogger(AppView.class);
+	
+	private Composite parent;
+
+	private Label selectedLabel;
+	private Button updateAppsButton;
+	private Button killAppButton;
+	private Button sortButton; //Sort by App or Module
+	private Table appTable;
+	private Table blockTable;
+	
+	private Display display;
+	private ApplicationInformation selectedApp;
+	private ApplicationManager appManager;
+	
+	/**
+	 * Horizontal space between cells.
+	 */
+	public static final int HORIZONTAL_SPACE = 10;
+
+	/**
+	 * Vertical space between cells.
+	 */
+	public static final int VERTICAL_SPACE = 5;
+	
+	public static final String SORT_APP = "Sort by Apps";
+	
+	public static final String SORT_MOD = "Sort by Modules";
 
 	// Used to map FunctionBlocks on table items
 	// private Map<UUID, TableItem> map = new HashMap<UUID, TableItem>();
@@ -27,26 +82,214 @@ public class AppView extends ViewPart {
 	}
 
 	public void setFocus() {
-		label.setFocus();
 	}
 
+	@Override
+	public void init(IViewSite site, IMemento memento) throws PartInitException {
+		LOGGER.entry(site, memento);
+		super.init(site, memento);
+		display = Display.getCurrent();
+		if (display == null) {
+			display = Display.getDefault();
+			LOGGER.trace("Display.getCurrent() returned null, using Display.getDefault(): {}", display);
+		}
+		appManager = ServerManager.getDefault().getApplicationManager();
+		appManager.addApplicationListener(this);
+		LOGGER.exit();
+	}
+	
+	@Override
+	public void dispose(){
+		appManager.removeApplicationListener(this);
+	}
+	
 	public void createPartControl(Composite parent) {
-		label = new Label(parent, 0);
-		label.setText("Applications");
-		label.setToolTipText("Shows running function blocks");
+		initParent(parent);
 
-		createFunctionBlockTable(parent);
+		createUpdateAppsButton();
+		createKillAppButton();
+		createSortButton();
+		createAppTable();
+		createBlockTable();
 	}
 
-	private void createFunctionBlockTable(Composite parent) {
-		Table functTable = new Table(parent, 0);
-		functTable.setLinesVisible(true);
-		functTable.setHeaderVisible(true);
+	private void initParent(final Composite parent) {
+		this.parent = parent;
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 3;
+		layout.horizontalSpacing = HORIZONTAL_SPACE;
+		layout.verticalSpacing = VERTICAL_SPACE;
+		this.parent.setLayout(layout);
+	}
 
-		TableColumn column = new TableColumn(functTable, SWT.NONE);
+	private void createUpdateAppsButton(){
+		GridData data = new GridData();
+		data.horizontalAlignment = SWT.FILL;
+		updateAppsButton = new Button(parent, SWT.NONE);
+		updateAppsButton.setLayoutData(data);
+		updateAppsButton.setText("Update Applications");
+		updateAppsButton.addSelectionListener(new SelectionAdapter(){
+			@Override
+			public void widgetSelected(SelectionEvent e){
+				AppView.this.updateAppList();
+			}
+		});
+	}
+	
+	private void createKillAppButton(){
+		GridData data = new GridData();
+		data.horizontalAlignment = SWT.BEGINNING;
+		killAppButton = new Button(parent, SWT.NONE);
+		killAppButton.setLayoutData(data);
+		killAppButton.setText("Kill Application");
+		killAppButton.setEnabled(false);
+		killAppButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				AppView.this.killApp();
+			}
+		});
+	}
+	
+	private void createSortButton(){
+		GridData data = new GridData();
+		data.horizontalAlignment = SWT.BEGINNING;
+		sortButton = new Button(parent, SWT.NONE);
+		sortButton.setText(SORT_APP);
+		sortButton.setEnabled(false);
+		sortButton.addSelectionListener(new SelectionAdapter(){
+			@Override
+			public void widgetSelected(SelectionEvent e){
+				AppView.this.sort();
+			}
+		});
+	}
+	
+	private void createSelectedLabel(){
+		selectedLabel = new Label(parent, SWT.NONE);
+		selectedLabel.setText("<select application>");
+		selectedLabel.pack();
+	}
+	
+	private void createAppTable(){
+		GridData data = new GridData();
+		data.horizontalAlignment = SWT.FILL;
+		data.verticalAlignment = SWT.FILL;
+		data.horizontalSpan = 2;
+		data.grabExcessHorizontalSpace = true;
+		data.grabExcessVerticalSpace = true;
+		appTable = new Table(parent, SWT.NONE);
+		appTable.setHeaderVisible(true);
+		appTable.setLinesVisible(true);
+		appTable.setLayoutData(data);
+		TableColumn column0 = new TableColumn(appTable, SWT.NONE);
+		column0.setText("Applications:");
+		appTable.getColumn(0).pack();
+		
+		appTable.addSelectionListener(new SelectionAdapter(){
+			@Override
+			public void widgetSelected(SelectionEvent e){
+				AppView.this.appSelected();
+			}
+		});
+		
+	}
+
+	private void createBlockTable() {
+		GridData data = new GridData();
+		data.horizontalAlignment = SWT.FILL;
+		data.verticalAlignment = SWT.FILL;
+		data.grabExcessHorizontalSpace = true;
+		data.grabExcessVerticalSpace = true;
+		
+		blockTable = new Table(parent, 0);
+		blockTable.setLayoutData(data);
+		blockTable.setLinesVisible(true);
+		blockTable.setHeaderVisible(true);
+
+		TableColumn column = new TableColumn(blockTable, SWT.NONE);
 		column.setText("Running Function Blocks");
-		functTable.setToolTipText("Currently running function blocks");
-		functTable.getColumn(0).pack();
+		blockTable.setToolTipText("Currently running function blocks");
+		blockTable.getColumn(0).pack();
+	}
+	
+	/**
+	 * Updates list of currently running applications.
+	 */
+	/**
+	 * Updates Information on which applications are running on a module.
+	 * @return
+	 */
+	private synchronized void updateAppList(){
+		//TODO: Remove outdated apps.
+		appManager.updateAppInfo();
+	}
+
+	
+	/**
+	 * Kills an Application.
+	 */
+	private void killApp(){
+		//TODO: Kill app, remove from table. update?
+	}
+	
+	private void sort(){
+		if (SORT_APP.equals(sortButton.getText())){
+			sortButton.setText(SORT_MOD);
+		}
+		else if (SORT_MOD.equals(sortButton.getText())){
+			sortButton.setText(SORT_APP);
+		}
+		//TODO: Sort by Application / Module.
+		/**
+		 * If sorted by app: Table on the left displays all applications. If one selected: show modules and Blocks.
+		 * If sorted by module: Table on the left displays all modules. If one selected: show all apps and Blocks.
+		 */
+	}
+	
+	private void appSelected(){
+		TableItem[] items = appTable.getSelection();
+		if (items.length == 1){
+			TableItem selectedItem = items[0];
+			selectedLabel.setText(selectedItem.getText(0));
+		}
+		killAppButton.setEnabled(true);
+	}
+
+	@Override
+	public void serverOnline() {
+		LOGGER.entry();
+		display.asyncExec(new Runnable() {
+			@Override
+			public void run() {
+				updateAppsButton.setEnabled(true);
+			}
+		});
+		LOGGER.exit();
+	}
+
+	@Override
+	public void serverOffline() {
+		LOGGER.entry();
+		display.asyncExec(new Runnable() {
+			@Override
+			public void run() {
+				killAppButton.setEnabled(false);
+				updateAppsButton.setEnabled(false);
+				sortButton.setEnabled(false);
+			}
+		});
+		LOGGER.exit();
+	}
+
+	@Override
+	public void applicationsResolved(Collection<ApplicationInformation> apps) {
+		System.out.println("bla");
+		for (ApplicationInformation info : apps){
+			System.out.println(info.getName());
+		}
+		//TODO: Display changes.
+		
 	}
 
 }
