adbb98340b585ae09c29a04484669361c975a1e3
Philipp Adolf <adolf@teco.edu>
2013-10-07 17:26:03 +0200

    Add HelloMessageHandler
    Handles incoming HelloMessages. If the handler is the master it checks to see
    if there is an existing connection and decides whether or not the new one
    should be dropped.

--- DND/tests/edu/teco/dnd/network/tcp/tests/TCPTests.java
+++ DND/tests/edu/teco/dnd/network/tcp/tests/TCPTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ ServerChannelManagerTest.class, ClientChannelManagerTest.class })
+@SuiteClasses({ ServerChannelManagerTest.class, ClientChannelManagerTest.class, HelloMessageHandlerTest.class })
 public class TCPTests {
 
 }
