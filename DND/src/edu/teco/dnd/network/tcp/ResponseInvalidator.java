493770d8b327b04db0782b871db69be6aa56aad4
Philipp Adolf <adolf@teco.edu>
2013-10-09 17:02:04 +0200

    Add ResponseInvalidator

---
+++ DND/src/edu/teco/dnd/network/tcp/ResponseInvalidator.java
@@ -0,0 +1,20 @@
+package edu.teco.dnd.network.tcp;
+
+import edu.teco.dnd.network.tcp.ResponseFutureManager.ResponseFutureNotifier;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+
+public class ResponseInvalidator implements ChannelFutureListener {
+	private final ResponseFutureNotifier responseFutureNotifier;
+	
+	public ResponseInvalidator(final ResponseFutureNotifier responseFutureNotifier) {
+		this.responseFutureNotifier = responseFutureNotifier;
+	}
+	
+	@Override
+	public void operationComplete(final ChannelFuture future) {
+		if (future.isDone() && !future.isSuccess()) {
+			responseFutureNotifier.setFailure0(future.cause());
+		}
+	}
+}
