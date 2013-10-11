63324548e2a141bf1e54700381076e3921e4562b
Philipp Adolf <adolf@teco.edu>
2013-10-11 15:33:25 +0200

    Add remote UUID to ConnectionEstablishedMessage

--- DND/src/edu/teco/dnd/network/messages/ConnectionEstablishedMessage.java
+++ DND/src/edu/teco/dnd/network/messages/ConnectionEstablishedMessage.java
@@ -1,5 +1,7 @@
 package edu.teco.dnd.network.messages;
 
+import java.util.UUID;
+
 /**
  * Message that signals that the sender has accepted the connection.
  * 
@@ -10,9 +12,20 @@ public class ConnectionEstablishedMessage extends Message {
 	 * The type of this message.
 	 */
 	public static final String MESSAGE_TYPE = "connection established";
+	
+	private final UUID remoteUUID;
+	
+	public ConnectionEstablishedMessage(final UUID remoteUUID) {
+		super();
+		this.remoteUUID = remoteUUID;
+	}
 
 	@Override
 	public String toString() {
-		return "ConnectionEstablishedMessage[]";
+		return "ConnectionEstablishedMessage[uuid=" + getUUID() + ",remoteUUID=" + remoteUUID + "]";
+	}
+
+	public UUID getRemoteUUID() {
+		return remoteUUID;
 	}
 }
