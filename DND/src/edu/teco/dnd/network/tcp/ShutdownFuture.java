172728bc65cbe2292a13f5280cdee129c69d9e67
Philipp Adolf <adolf@teco.edu>
2013-10-16 16:38:55 +0200

    Add JavaDoc for ShutdownFuture

--- DND/src/edu/teco/dnd/network/tcp/ShutdownFuture.java
+++ DND/src/edu/teco/dnd/network/tcp/ShutdownFuture.java
@@ -2,7 +2,15 @@ package edu.teco.dnd.network.tcp;
 
 import edu.teco.dnd.util.DefaultFutureNotifier;
 
+/**
+ * A future that is used by {@link TCPConnectionManager} to signal when it has finished shutting down.
+ * 
+ * @author Philipp Adolf
+ */
 public class ShutdownFuture extends DefaultFutureNotifier<Void> {
+	/**
+	 * Called by {@link TCPConnectionManager} when it has shut down.
+	 */
 	void setComplete() {
 		setSuccess(null);
 	}
